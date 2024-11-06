a8d3718836dcb62d54ef52fd446866ce00311c4f
LizaFlux <troshinalizzza@gmail.com>
2024-11-06 03:29:06 +0300

    _index.ts

---
+++ src/translations/resources/_index.ts
@@ -0,0 +1,15 @@
+import common from './Общее.json';
+import welcome from './Добро пожаловать.json';
+import cr from './СоздатьВосстановить.json';
+import login from './Авторизация.json';
+import home from './Домашняя_страница.json';
+import send from './Отправить.json';
+
+export default {
+  common,
+  welcome,
+  cr,
+  login,
+  home,
+  send,
+};
